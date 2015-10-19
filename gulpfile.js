651c1f4ebd6c4c2fcb3a7409e4a6bd838dde996d
sintu-singh <ssingh172@sapient.com>
2015-10-19 22:36:28 +0530

    Gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var compass = require('gulp-compass');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('javascript', function() {
   return gulp.src('dev/javascript/*.js')
@@ -8,4 +11,14 @@ gulp.task('javascript', function() {
       .pipe(concat('javascript.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dev/assets/js/'));
+});
+
+gulp.task('compass', function() {
+  gulp.src('dev/sass/style.scss')
+    .pipe(compass({
+      css: 'assets/css',
+      sass: 'dev/sass/'
+    }))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('assets/css'));
 });
